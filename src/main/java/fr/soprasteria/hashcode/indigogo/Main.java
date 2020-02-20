1b9c2fd8886549f2d108d7d309d373c6be4e57d2
slim <stephane.lim@soprasteria.com>
2020-02-20 20:28:07 +0100

    Mise à jour du lecteur

--- src/main/java/fr/soprasteria/hashcode/indigogo/Main.java
+++ src/main/java/fr/soprasteria/hashcode/indigogo/Main.java
@@ -1,5 +1,10 @@
 package fr.soprasteria.hashcode.indigogo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.soprasteria.hashcode.indigogo.model.Context;
+import fr.soprasteria.hashcode.indigogo.model.Librairie;
 import fr.soprasteria.hashcode.indigogo.reader.Reader;
 import fr.soprasteria.hashcode.indigogo.writer.Writer;
 
@@ -14,7 +19,30 @@ public class Main {
 		
 		Reader.read(BASENAME + args[0]);
 		
-		Writer.write(BASENAME_OUT + args[0]);
-	}
+		Context ctx = new Context();
+		List<Librairie> listeLibrairies = new ArrayList<Librairie>();
+		Librairie librairie1 = new Librairie();
+		librairie1.setIdx("0");
+		List<String> listeLivres1 = new ArrayList<String>();
+		listeLivres1.add("1");
+		listeLivres1.add("2");
+		librairie1.setListeIndexLivres(listeLivres1);
+		
+		Librairie librairie2 = new Librairie();
+		librairie2.setIdx("1");
+		
+		List<String> listeLivres2 = new ArrayList<String>();
+		listeLivres2.add("3");
+		listeLivres2.add("4");
+		listeLivres2.add("5");
+		librairie2.setListeIndexLivres(listeLivres2);
+		
+		listeLibrairies.add(librairie1);
+		listeLibrairies.add(librairie2);
+		
+		ctx.setLibrairies(listeLibrairies);
+		
+		Writer.write(BASENAME_OUT + args[0], ctx);
+	} 
 
 }
--- src/main/java/fr/soprasteria/hashcode/indigogo/writer/Writer.java
+++ src/main/java/fr/soprasteria/hashcode/indigogo/writer/Writer.java
@@ -6,31 +6,39 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
+import fr.soprasteria.hashcode.asteam.model.Ride;
+import fr.soprasteria.hashcode.indigogo.model.Context;
+import fr.soprasteria.hashcode.indigogo.model.Librairie;
+
 public final class Writer {
 
 	private static final String OUT_EXTENSION = ".out";
 
-	public static void write(final String filename) {
+	public static void write(final String filename, final Context context) {
 
 		final String filenameWithExtension = filename + OUT_EXTENSION;
 		File file = new File(filenameWithExtension);
 		FileOutputStream fileOutputStream;
 		BufferedWriter bufferedWriter = null;
+		
 		try {
 			fileOutputStream = new FileOutputStream(file);
 
 			bufferedWriter = new BufferedWriter(new OutputStreamWriter(
 					fileOutputStream));
 
-			// for(Vehicule vehicule : vehicules) {
-			//
-			// bufferedWriter.write(String.valueOf(vehicule.getRides().size()));
-			// for(Ride ride : vehicule.getRides()) {
-			// bufferedWriter.write(" " + ride.getIndex());
-			// }
-			//
-			// bufferedWriter.newLine();
-			// }
+			bufferedWriter.write(String.valueOf(context.getLibrairies().size()));
+			bufferedWriter.newLine();
+
+			for (Librairie librarie : context.getLibrairies()) {
+				bufferedWriter
+						.write(String.valueOf(librarie.getIdx() + " " + librarie.getListeIndexLivres().size()));
+				bufferedWriter.newLine();
+				for (String indexLivre : librarie.getListeIndexLivres()) {
+					bufferedWriter.write(indexLivre + " ");
+				}
+				bufferedWriter.newLine();
+			}
 
 		} catch (IOException e) {
 			e.printStackTrace();

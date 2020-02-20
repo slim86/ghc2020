bc76d8094d1cb0ac5b60e313d96098215d45928f
slim <stephane.lim@soprasteria.com>
2020-02-20 21:29:49 +0100

    Test

--- src/main/java/fr/soprasteria/hashcode/indigogo/reader/Reader.java
+++ src/main/java/fr/soprasteria/hashcode/indigogo/reader/Reader.java
@@ -6,7 +6,10 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import fr.soprasteria.hashcode.indigogo.model.Context;
@@ -17,7 +20,9 @@ public final class Reader {
 
 	private static final String IN_EXTENTION = ".in";
 	
-	public static void read(final String fileName) {
+	public static Context read(final String fileName) {
+		
+		Context ctxt = new Context();
 		
 		final String fileNameWithExtension = fileName + IN_EXTENTION;
 
@@ -32,7 +37,7 @@ public final class Reader {
 					+ lines.size());
 
 			String[] infosFirstLine = lines.get(0).split(" ");
-			Context ctxt = new Context();
+			
 			ctxt.setNbLivres(Integer.parseInt(infosFirstLine[0]));
 			ctxt.setNbLibrairies(Integer.parseInt(infosFirstLine[1]));
 			ctxt.setNbJours(Integer.parseInt(infosFirstLine[2]));
@@ -42,17 +47,22 @@ public final class Reader {
 			List<Livre> listeLivres = new ArrayList<Livre>();
 			String[] scoreLivres = lines.get(1).split(" ");
 			int idxLivre = 0;
+			Map<String, Integer> mapScore = new HashMap<String, Integer>();
 			for (String scoreLivre : scoreLivres) {
 				final Livre livre = new Livre();
 				livre.setId(Integer.toString(idxLivre));
 				livre.setScore(Integer.parseInt(scoreLivre));
 				listeLivres.add(livre);
+				mapScore.put(Integer.toString(idxLivre), Integer.parseInt(scoreLivre));
+				idxLivre++;
 			}
 			
 			final List<Librairie> listeLibrairie = new ArrayList<Librairie>();
+			int index = 0;
 			for (int idxLibrairie = 0; idxLibrairie < ((ctxt.getNbLibrairies() *2)); idxLibrairie++) {
 				final String[] infosLibrairie = lines.get(idxLibrairie + 2).split(" ");
 				final Librairie librairie = new Librairie();
+				librairie.setIdx(Integer.toString(index));
 				librairie.setNbLivres(Integer.parseInt(infosLibrairie[0]));
 				librairie.setNbJoursOuverture(Integer.parseInt(infosLibrairie[1]));
 				librairie.setCapaciteTraitement(Integer.parseInt(infosLibrairie[2]));
@@ -60,16 +70,21 @@ public final class Reader {
 				final String[] infosLibrairieIdxLivres = lines.get(idxLibrairie + 3).split(" ");
 				librairie.setListeIndexLivres(Arrays.asList(infosLibrairieIdxLivres));
 				listeLibrairie.add(librairie);
-				idxLibrairie = idxLibrairie + 1;
+				idxLibrairie++;
+				index++;
 			}
 			
 			System.out.println("Liste des librairies :");
 			listeLibrairie.forEach(item->System.out.println(item));
+//			Collections.reverse(listeLibrairie);
+			ctxt.setLibrairies(listeLibrairie);
+			
 			
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
+		return ctxt;
 	}
 }
